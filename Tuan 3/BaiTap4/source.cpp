5766b45bb07c2bfd881847eb0e8d4a8d88f81a43
Lê Hà Gia Bảo <20520141@gm.uit.edu.vn>
2021-10-07 16:35:55 +0700

    Create source.cpp

---
+++ Tuan 3/BaiTap4/source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"ThoiGian.h"
+#include"ThoiGian.cpp"
+using namespace std;
+
+int main()
+{
+    ThoiGian DuLieu;
+    DuLieu.Nhap();
+    DuLieu.XuLyThoiGian();
+    DuLieu.Xuat();
+    return 0;
+}
