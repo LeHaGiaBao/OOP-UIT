fa1f032b2dbe50ff69eb13bd504e9b6442a7303d
Lê Hà Gia Bảo <20520141@gm.uit.edu.vn>
2021-10-07 16:37:07 +0700

    Create source.cpp

---
+++ Tuan 5/BaiTap1/source.cpp
@@ -0,0 +1,16 @@
+#include"NhanVien.h"
+#include"NhanVien.cpp"
+#include"NhanVienSanXuat.h"
+#include"NhanVienSanXuat.cpp"
+#include"NhanVienVanPhong.h"
+#include"NhanVienVanPhong.cpp"
+#include"QuanLyNhanVien.h"
+#include"QuanLyNhanVien.cpp"
+
+int main()
+{
+    QuanLyNhanVien DuLieu;
+    DuLieu.Nhap();
+    DuLieu.Xuat();
+    return 0;
+}
