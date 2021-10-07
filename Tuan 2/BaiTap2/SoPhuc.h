435ba22f5c2aadf1bee2c547d0ae415163fd4915
Lê Hà Gia Bảo <20520141@gm.uit.edu.vn>
2021-10-07 16:34:46 +0700

    Create SoPhuc.h

---
+++ Tuan 2/BaiTap2/SoPhuc.h
@@ -0,0 +1,13 @@
+class SoPhuc
+{
+    private:
+        double PhanThuc;
+        double PhanAo;
+    public:
+        void Nhap();
+        void Xuat();
+        void Cong(SoPhuc sophuc);
+        void Tru(SoPhuc sophuc);
+        void Nhan(SoPhuc sophuc);
+        void Chia(SoPhuc sophuc);
+};
