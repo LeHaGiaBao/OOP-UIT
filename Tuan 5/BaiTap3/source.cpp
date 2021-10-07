09f1f14d9cd2e86120e27b29ec72fdc0e354a386
Lê Hà Gia Bảo <20520141@gm.uit.edu.vn>
2021-10-07 16:37:54 +0700

    Create source.cpp

---
+++ Tuan 5/BaiTap3/source.cpp
@@ -0,0 +1,16 @@
+#include"NhaThue.h"
+#include"NhaThue.cpp"
+#include"ChungCu.h"
+#include"ChungCu.cpp"
+#include"BinhThuong.h"
+#include"BinhThuong.cpp"
+#include"CongTyABC.h"
+#include"CongTyABC.cpp"
+
+int main()
+{
+    CongTyABC DuLieu;
+    DuLieu.Nhap();
+    DuLieu.Xuat();
+    return 0;
+}
