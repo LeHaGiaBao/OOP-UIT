f0a799932ad2905ee8e1ae8e4e68793b07973cad
Lê Hà Gia Bảo <20520141@gm.uit.edu.vn>
2021-10-07 16:34:55 +0700

    Create Diem.h

---
+++ Tuan 2/BaiTap4/Diem.h
@@ -0,0 +1,11 @@
+class Diem
+{
+private:
+        double HoanhDo;
+        double TungDo;
+    public:
+        void nhapdiem(Diem& a);
+        void xuatdiem(Diem& a);
+        void tinhtien(Diem& a);
+        void quay(Diem& a);
+};
